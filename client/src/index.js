1571f6b990fdcc68990a166195100437c3f68e1d
Yagizhan Avci <ygzhn14@gmail.com>
2019-09-16 16:01:43 +0300

    changed index.html and fontawesome

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(
   <Provider store={store}>
